70fb1cf187256fb42e1952609db7e6579c9540a7
Hize18 <98449664+Hize18@users.noreply.github.com>
2026-06-07 16:49:19 +0900

    test: add API integration tests

---
+++ src/test/java/com/sprint/mission/discodeit/integration/ChannelApiIntegrationTest.java
@@ -0,0 +1,148 @@
+package com.sprint.mission.discodeit.integration;
+
+import static org.hamcrest.Matchers.hasItems;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint.mission.discodeit.dto.channel.ChannelDto;
+import com.sprint.mission.discodeit.dto.channel.ChannelUpdateRequest;
+import com.sprint.mission.discodeit.dto.channel.PrivateChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.channel.PublicChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.user.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.user.UserDto;
+import com.sprint.mission.discodeit.entity.ChannelType;
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.UserService;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+public class ChannelApiIntegrationTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @Autowired
+  private UserService userService;
+
+  @Autowired
+  private ChannelService channelService;
+
+  private UserDto userDto;
+  private ChannelDto publicChannelDto;
+  private ChannelDto privateChannelDto;
+
+  @BeforeEach
+  public void setup() {
+    userDto = userService.create(
+        new UserCreateRequest("test_user", "test@test.com", "password1!"),
+        Optional.empty()
+    );
+
+    publicChannelDto = channelService.create(
+        new PublicChannelCreateRequest("channel", "description")
+    );
+
+    privateChannelDto = channelService.create(
+        new PrivateChannelCreateRequest(
+            List.of(userDto.id())
+        )
+    );
+  }
+
+  @Test
+  @DisplayName("create_public_channel")
+  public void create_public_channel() throws Exception {
+    PublicChannelCreateRequest publicChannelCreateRequest = new PublicChannelCreateRequest(
+        "public_channel",
+        "description"
+    );
+
+    mockMvc.perform(post("/api/channels/public")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(publicChannelCreateRequest)))
+        .andExpect(jsonPath("$.type").value(ChannelType.PUBLIC.toString()))
+        .andExpect(jsonPath("$.name").value(publicChannelCreateRequest.name()))
+        .andExpect(jsonPath("$.description").value(publicChannelCreateRequest.description()))
+        .andExpect(jsonPath("$.lastMessageAt").doesNotExist());
+  }
+
+  @Test
+  @DisplayName("create_private_channel")
+  public void create_private_channel() throws Exception {
+    PrivateChannelCreateRequest privateChannelCreateRequest = new PrivateChannelCreateRequest(
+        List.of(userDto.id())
+    );
+
+    mockMvc.perform(post("/api/channels/private")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(privateChannelCreateRequest)))
+        .andExpect(jsonPath("$.type").value(ChannelType.PRIVATE.toString()))
+        .andExpect(jsonPath("$.participants[0].username").value(userDto.username()));
+  }
+
+  @Test
+  @DisplayName("find_all_by_user_id")
+  public void find_all_by_user_id() throws Exception {
+    mockMvc.perform(get("/api/channels")
+            .param("userId", String.valueOf(userDto.id())))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.size()").value(2))
+        .andExpect(jsonPath("$[*].id").value(hasItems(publicChannelDto.id().toString())));
+  }
+
+  @Test
+  @DisplayName("update_public_channel")
+  public void update_public_channel() throws Exception {
+    ChannelUpdateRequest channelUpdateRequest = new ChannelUpdateRequest(
+        "newName",
+        "newDescription"
+    );
+
+    mockMvc.perform(patch("/api/channels/{channelId}", publicChannelDto.id())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(channelUpdateRequest)))
+        .andExpect(jsonPath("$.type").value(ChannelType.PUBLIC.toString()))
+        .andExpect(jsonPath("$.name").value(channelUpdateRequest.newName()))
+        .andExpect(jsonPath("$.description").value(channelUpdateRequest.newDescription()))
+        .andExpect(jsonPath("$.lastMessageAt").doesNotExist());
+  }
+
+  @Test
+  @DisplayName("delete_channel")
+  public void delete_channel() throws Exception {
+    mockMvc.perform(get("/api/channels")
+            .param("userId", String.valueOf(userDto.id())))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.size()").value(2));
+
+    mockMvc.perform(delete("/api/channels/{channelId}", publicChannelDto.id()))
+        .andExpect(status().isNoContent());
+
+    mockMvc.perform(get("/api/channels")
+            .param("userId", String.valueOf(userDto.id())))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.size()").value(1));
+  }
+}
---
+++ src/test/java/com/sprint/mission/discodeit/integration/UserApiIntegrationTest.java
@@ -0,0 +1,147 @@
+package com.sprint.mission.discodeit.integration;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint.mission.discodeit.dto.user.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.user.UserDto;
+import com.sprint.mission.discodeit.dto.user.UserUpdateRequest;
+import com.sprint.mission.discodeit.service.UserService;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+public class UserApiIntegrationTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @Autowired
+  private UserService userService;
+
+  private UserDto userDto;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    userDto = userService.create(
+        new UserCreateRequest("test1", "test1@test.com", "password1!"),
+        Optional.empty()
+    );
+  }
+
+  @Test
+  @DisplayName("create_user")
+  public void createUser() throws Exception {
+    UserCreateRequest userCreateRequest = new UserCreateRequest(
+        "username",
+        "email@email.com",
+        "password123!"
+    );
+
+    MockMultipartFile userCreateRequestPart = new MockMultipartFile(
+        "userCreateRequest",
+        "",
+        "application/json",
+        objectMapper.writeValueAsBytes(userCreateRequest)
+    );
+
+    MockMultipartFile profilePart = new MockMultipartFile(
+        "profile",
+        "profile.png",
+        "image/png",
+        "profile".getBytes()
+    );
+
+    mockMvc.perform(multipart("/api/users")
+            .file(userCreateRequestPart)
+            .file(profilePart))
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.username").value(userCreateRequest.username()))
+        .andExpect(jsonPath("$.email").value(userCreateRequest.email()))
+        .andExpect(jsonPath("$.profile.fileName").value(profilePart.getOriginalFilename()))
+        .andExpect(jsonPath("$.profile.size").value(profilePart.getSize()))
+        .andExpect(jsonPath("$.profile.contentType").value(profilePart.getContentType()));
+  }
+
+  @Test
+  @DisplayName("find_all")
+  public void findAll() throws Exception {
+    UserDto userDto1 = userService.create(
+        new UserCreateRequest("test2", "test2@test.com", "password2!"),
+        Optional.empty()
+    );
+
+    mockMvc.perform(get("/api/users"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.size()").value(2))
+        .andExpect(jsonPath("$[0].id").value(userDto.id().toString()))
+        .andExpect(jsonPath("$[0].username").value(userDto.username()))
+        .andExpect(jsonPath("$[0].email").value(userDto.email()))
+        .andExpect(jsonPath("$[1].id").value(userDto1.id().toString()))
+        .andExpect(jsonPath("$[1].username").value(userDto1.username()))
+        .andExpect(jsonPath("$[1].email").value(userDto1.email()));
+  }
+
+  @Test
+  @DisplayName("update_user")
+  public void updateUser() throws Exception {
+    UserUpdateRequest userUpdateRequest = new UserUpdateRequest(
+        "updated_name",
+        "updated_email@update.com",
+        "updated_password1!"
+    );
+
+    MockMultipartFile userUpdateRequestPart = new MockMultipartFile(
+        "userUpdateRequest",
+        "",
+        "application/json",
+        objectMapper.writeValueAsBytes(userUpdateRequest)
+    );
+
+    mockMvc.perform(multipart("/api/users/{userId}", userDto.id())
+            .file(userUpdateRequestPart)
+            .with(request -> {
+              request.setMethod("PATCH");
+              return request;
+            }))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.id").value(userDto.id().toString()))
+        .andExpect(jsonPath("$.username").value(userUpdateRequest.newUsername()))
+        .andExpect(jsonPath("$.email").value(userUpdateRequest.newEmail()));
+
+  }
+
+  @Test
+  @DisplayName("delete_user")
+  public void deleteUser() throws Exception {
+    mockMvc.perform(get("/api/users"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.size()").value(1));
+
+    mockMvc.perform(delete("/api/users/{userId}", userDto.id()))
+        .andExpect(status().isNoContent());
+
+    mockMvc.perform(get("/api/users"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.size()").value(0));
+  }
+}
